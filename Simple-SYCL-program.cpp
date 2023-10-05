d8098e617872e1b998e07272dc7fa6dabebcfbd1
leXRishi32 <93484836+leXRishi32@users.noreply.github.com>
2023-10-06 05:06:33 +0530

    Create Simple-SYCL-program.cpp
    Simple SYCL program
    C++ with SYCL programs are single-source, meaning that the same
    translation unit (typically a source file and its headers) contains both the
    code that defines the compute kernels to be executed on SYCL devices and
    also the host code that orchestrates execution of those kernels

---
+++ Simple-SYCL-program.cpp
@@ -0,0 +1,24 @@
+#include <array>
+#include <iostream>
+#include <sycl/sycl.hpp>
+using namespace sycl;
+int main() {
+constexpr int size = 16;
+std::array<int, size> data;
+// Create queue on implementation-chosen default device
+queue q;
+// Create buffer using host allocated "data" array
+buffer B{data};
+q.submit([&](handler& h) {
+accessor A{B, h};
+h.parallel_for(size, [=](auto& idx) {
+A[idx] = idx;
+});
+});
+// Obtain access to buffer on the host
+// Will wait for device kernel to execute to generate data
+host_accessor A{B};
+for (int i = 0; i < size; i++)
+std::cout << "data[" << i << "] = " << A[i] << "\n";
+return 0;
+}
